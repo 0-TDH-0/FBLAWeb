513dd1ad2c490130f7a354d3b5aec57bb2c82209
pulle560 <97562961+pulle560@users.noreply.github.com>
2022-03-18 22:16:54 -0500

    marianos

--- location.js
+++ location.js
@@ -123,126 +123,126 @@ const loc = {
   
   
   const loc = {
-    Primo: {
-      phone: "847-336-3446",
-      address: "720 Milwaukee Ave, Gurnee, IL 60031",
-      img: "./images/Primo Italian Cuisine.jpg",
-      website: "https://primogurnee.com/",
-      timings: "11 am - 9 pm",
+    Marianos: {
+      phone: "866-742-6728",
+      address: "1720 N Milwaukee Ave, Vernon Hills, IL 60061",
+      img: "./images/Mariano's.jpg",
+      website: "http://www.marianos.com/",
+      timings: "6 am - 10 pm",
       about: ""
     },
-    Sarpino: {
-      phone: "847-223-5000",
-      address: "34484 US-45 Ste B, Third Lake, IL 60030",
-      img: "./images/Sarpino's Pizzeria.jpg",
-      website: "https://gurnee.gosarpinos.com/ordering/intro/",
-      timings: "10 am - 1 am",
+    Jewel_Osco: {
+      phone: "877-276-9637",
+      address: "922 Green Bay Rd, Waukegan, IL 60085",
+      img: "./images/Jewel-Osco.jpg",
+      website: "http://www.jewelosco.com/",
+      timings: "6 am - 12 am",
       about: ""
     },
-    Galati_Pizza: {
-      phone: "847-740-4222",
-      address: "1 Great America Parkway, Gurnee, IL 60031",
-      img: "./images/Gallati Pizza.jpg",
-      website: "https://www.galatipizza.com/",
-      timings: "3 pm - 11 pm",
+    Georges_Fresh_Market: {
+      phone: "224-656-5488",
+      address: "1490 Lewis Ave, North Chicago, IL 60064",
+      img: "./images/George's Fresh Market.jpg",
+      website: "https://www.georgesfreshmarket.com/",
+      timings: "7 am - 9 pm",
       about: ""
     },
-    Capones_Kitchen: {
-      phone: "847-249-1776",
-      address: "1 Great America Parkway, Gurnee, IL 60031",
-      img: "./images/Capone's Kitchen.png",
-      website: "https://www.sixflags.com/greatamerica/things-to-do/restaurants",
-      timings: "11 am - 10:30 pm",
+    Heinens_Grocery_Store: {
+      phone: "847-735-0026",
+      address: "201 Waukegan Rd, Lake Bluff, IL 60044",
+      img: "./images/Heinen's Grocery Store.jpg",
+      website: "http://www.heinens.com/",
+      timings: "8 am - 8 pm",
       about: ""
     },
-    Olive_Garden_Italian_Resturant: {
-      phone: "847-336-3628",
-      address: "5590 Northridge Dr, Gurnee, IL 60031",
-      img: "./images/Olive Garden Italian Resturant.jpg",
-      website: "https://www.olivegarden.com/home",
-      timings: "11 am - 10 pm",
+    Jims_Pantry_Food_Mart: {
+      phone: "847-623-6116",
+      address: "Waukegan, IL 60087",
+      img: "./images/Jim's Pantry Food Mart.jpg",
+      website: "https://nicelocal.com/illinois/shops/jims_pantry/",
+      timings: "9 am - 12 pm",
       about: ""
     },
-    Jesse_Oaks_Food_and_Drinks: {
-      phone: "847-223-2575",
-      address: "18490 W Old Gages Lake Rd, Gages Lake, IL 60030",
-      img: "./images/Jesse Oaks Food & Drink.jpg",
-      website: "https://www.jesseoaks.com/",
-      timings: "10 am - 1 am",
+    Full_Circle_Clothing: {
+      phone: "N/A",
+      address: "228 N Genesee St Suite D, Waukegan, IL 60085",
+      img: "./images/Full Circle Clothing.jpg",
+      website: "https://fullcircleclothing2020.com/",
+      timings: "1 pm - 7 pm",
       about: ""
     },
-    Flanagans_Bar_and_Grill: {
-      phone: "847-689-9062",
-      address: " 3201 Buckley Rd, North Chicago, IL 60064",
-      img: "./images/Flanagan's Bar & Grill.jpg",
-      website: "http://www.flanaganssportsbar.com/",
-      timings: "10 am - 1 am",
+    dds_DISCOUNT: {
+      phone: "847-599-8632",
+      address: "1535 N Lewis Ave, Waukegan, IL 60085",
+      img: "./images/dd's DISCOUNTS.jpg",
+      website: "https://www.ddsdiscounts.com/",
+      timings: "9 am - 10:30 pm",
       about: ""
     },
-    First_Draft_Grays_lake: {
-      phone: "(224) 541-4445",
-      address: "250 Center St, Grayslake, IL 60030",
-      img: "./images/First Draft Grayslake.jpg",
-      website: "http://firstdraftgrayslake.net/",
-      timings: "12 pm - 11 pm",
+    H_and_M: {
+      phone: "855-466-7467",
+      address: "109 Hawthorn Center, Vernon Hills, IL 60061",
+      img: "./images/H&M.jpg",
+      website: "http://www.hm.com/",
+      timings: "11 am - 8 pm",
       about: ""
     },
-    Culvers: {
-      phone: "847-662-9666",
-      address: "1505 Nations Dr, Gurnee, IL 60031",
-      img: "./images/Culver's.jpg",
-      website: "http://www.culvers.com/",
-      timings: "10:30 am - 10 pm",
+    Ross_Dress_for_Less: {
+      phone: "847-599-1405",
+      address: "1301 N Lewis Ave, Waukegan, IL 60085",
+      img: "./images/Ross Dress for Less.jpg",
+      website: "https://www.rossstores.com/",
+      timings: "10 am - 8 pm",
       about: ""
     },
-    The_Backyard_Steak_Pit: {
-      phone: "847-356-5200",
-      address: "1818 N Grandwood Dr, Gurnee, IL 60031",
-      img: "./images/The Backyard Steak Pit.jpg",
-      website: "https://thebackyardsteakpit.com//",
-      timings: "4 pm - 8 pm",
+    Windsor: {
+      phone: "847-855-7439",
+      address: "6170 Grand Ave Ste 529, Gurnee, IL 60031",
+      img: "./images/Windsor.jpg",
+      website: "https://www.windsorstore.com/pages/il-gurnee-gurnee-mills",
+      timings: "11 am - 8 pm",
       about: ""
     },
-    El_Atezca_Two: {
-      phone: "847-231-6512",
-      address: "821 Center St, Grayslake, IL 60030",
-      img: "./images/El Azteca II",
-      website: "https://www.elaztecagrayslake.com/",
-      timings: "10 am - 9 pm",
+    Lucys_Appliances: {
+      phone: "847-872-4922",
+      address: "3460 Sheridan Rd, Zion, IL 60099",
+      img: "./images/Lucy's Appliances.jpg",
+      website: "https://www.lucysappliances.com/",
+      timings: "9 am - 5 pm",
       about: ""
     },
-    Tacos_El_Norte_Gurnee: {
-      phone: "847-623-3034",
-      address: "401 N Riverside Dr #27, Gurnee, IL 60031",
-      img: "./images/Tacos El Norte Gurnee.jpg",
-      website: "http://www.tacoselnortegurnee.com/",
-      timings: "10 am - 10 pm",
+    Best_Buy: {
+      phone: "847-406-3914",
+      address: "6525 Grand Ave, Gurnee, IL 60031",
+      img: "./images/Best Buy.jpg",
+      website: "https://www.bestbuy.com/",
+      timings: "11 am - 7 pm",
       about: ""
     },
-    Abels_Cantina: {
-      phone: "847-548-2003",
-      address: "125 Center St, Grayslake, IL 60030",
-      img: "./images/Abel's Cantina.jpg",
-      website: "https://abelscantina.square.site/",
-      timings: "4 pm - 11 pm",
+    Aarons: {
+      phone: "847-625-1111",
+      address: "1403 N Lewis Ave, Waukegan, IL 60085",
+      img: "./images/Aaron's.jpg",
+      website: "http://www.aarons.com/",
+      timings: "10 am - 7 pm",
       about: ""
     },
-    Taqueria_Los_Comales: {
-      phone: "847-740-2606",
-      address: "809 W Rollins Rd, Round Lake Heights, IL 60073",
-      img: "./images/Taqueria Los Camales.jpg",
-      website: "https://www.loscomales.com/",
-      timings: "8 am - 12 am",
+    The_Home_Depot: {
+      phone: "847-625-1020",
+      address: "2001 Belvedere Rd, Waukegan, IL 60085",
+      img: "./images/The Home Depot.jpg",
+      website: "http://www.homedepot.com/",
+      timings: "6 am - 10 pm",
       about: ""
     },
-    Dulces_Margaritas: {
-      phone: "847-223-3505",
-      address: "1814 E Belvidere Rd, Grayslake, IL 60030",
-      img: "./images/Dulce Margaritas.jpg",
-      website: "https://dulcemargaritas.com//",
-      timings: "10 am - 9 pm",
+    Affordable_Appliance_Inc: {
+      phone: "847-625-1020",
+      address: "2001 Belvedere Rd, Waukegan, IL 60085",
+      img: "./images/Affordable Appliances, Inc.jpg",
+      website: "https://affordable-appliances-inc.business.site/",
+      timings: "9 am - 4 pm",
       about: ""
-    }
+    },
   };
 
 
